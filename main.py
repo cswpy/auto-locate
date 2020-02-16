dce347ce8acbd4cfe2387f09e7013ee963285895
Phillip Wong <hncswpy@gmail.com>
2020-02-16 19:51:26 +0400

    --- Auto-Git Commit ---

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import get_access_point
+from flask import Flask
 
-get_access_point.get_location()
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    #response, last_location, last_time = get_access_point.get_location()
+    #ans = response + '. He is last seen ' + last_location + ' at '+last_time
+    #print(ans)
+    return "LOL"
+
+if(__name__=='__main__'):
+    app.run()
